965020851d114e7de1419cd6b12b393642a9670c
Pavlin <forex.xard@gmail.com>
2021-07-28 16:55:42 -0400

    Created React app added collors

---
+++ src/Pallet.js
@@ -0,0 +1,16 @@
+
+const Palet = () => {
+
+    return (
+        <div className="Palette">
+        {/* Navbar goes here */}
+            <div className="Palette-colors">
+            {/* Color boxes */}
+
+            </div>
+            {/* Footer goes here */}
+        </div>
+    )
+}
+
+export default Palet;
